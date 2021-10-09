f6b902f5085304c842e6996443c878b8d9ab8dfd
Yhëhtozr <conlang2012@outlook.com>
2021-10-10 08:02:34 +0900

    site styling reapplication 211009

--- app/javascript/articles/components/CommentsCount.jsx
+++ app/javascript/articles/components/CommentsCount.jsx
@@ -14,7 +14,10 @@ export const CommentsCount = ({ count, articlePath, articleTitle }) => {
       <path d="M10.5 5h3a6 6 0 110 12v2.625c-3.75-1.5-9-3.75-9-8.625a6 6 0 016-6zM12 15.5h1.5a4.501 4.501 0 001.722-8.657A4.5 4.5 0 0013.5 6.5h-3A4.5 4.5 0 006 11c0 2.707 1.846 4.475 6 6.36V15.5z" />
     </svg>
   );
-  const commentsAriaLabelText = i18next.t('comments.aria_label', { title: articleTitle, num: count });
+  const commentsAriaLabelText = i18next.t('comments.aria_label', {
+    title: articleTitle,
+    num: count,
+  });
 
   if (count > 0) {
     return (
@@ -25,16 +28,11 @@ export const CommentsCount = ({ count, articlePath, articleTitle }) => {
         url={`${articlePath}#comments`}
         icon={commentsSVG}
         tagName="a"
+        className="crayons-reaction"
         aria-label={commentsAriaLabelText}
       >
         <span title={i18next.t('comments.number')}>
-          {count}
-          <span className="hidden s:inline">
-            &nbsp;
-            {`${
-              count > 1 ? `${locale('core.comment')}s` : locale('core.comment')
-            }`}
-          </span>
+          <span className="crayons-reaction__count">{count}</span>
         </span>
       </Button>
     );
@@ -49,6 +47,7 @@ export const CommentsCount = ({ count, articlePath, articleTitle }) => {
         icon={commentsSVG}
         tagName="a"
         data-testid="add-a-comment"
+        className="crayons-reaction"
         aria-label={commentsAriaLabelText}
       >
         <span className="crayons-reaction__count">
