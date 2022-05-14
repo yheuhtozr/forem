0058f7782b604ec3e18b90c51e73a7b31bad6daa
Yhëhtozr <conlang2012@outlook.com>
2022-05-14 13:11:22 +0900

    site function: OGP image implementation
    Co-authored-by: ゆりと架鉄とデザインと <yuyuyu080718@gmail.com>

--- app/controllers/social_previews_controller.rb
+++ app/controllers/social_previews_controller.rb
@@ -5,18 +5,18 @@ class SocialPreviewsController < ApplicationController
 
   def article
     @article = Article.find(params[:id])
-    @tag_badges = Badge.where(id: Tag.where(name: @article.decorate.cached_tag_list_array).select(:badge_id))
-    not_found unless @article.published
+    @lang = @article.base_lang
 
-    template = @article.tags
-      .where.not(social_preview_template: nil)
-      .where.not(social_preview_template: "article")
-      .select(:social_preview_template).first&.social_preview_template
-
-    # make sure that the template exists
-    template = "article" unless Tag.social_preview_templates.include?(template)
-
-    set_respond "social_previews/articles/#{template}"
+    respond_to do |format|
+      template = "social_previews/articles/migdal_ogp"
+      format.html do
+        render template, layout: false
+      end
+      format.png do
+        html = render_to_string template, formats: :html, layout: false
+        redirect_to OgpGeneration.new(html, params[:id]).url
+      end
+    end
   end
 
   def user
---
+++ app/services/ogp_generation.rb
@@ -0,0 +1,42 @@
+class OgpGeneration
+  require "fileutils"
+  require "playwright"
+
+  ROUTES = %w[uploads articles ogp].freeze
+  CACHE_PATH = "cache.html".freeze
+  IMAGE_PATH = "image.png".freeze
+  DIR_PATH = Rails.root.join "public", *ROUTES
+
+  def initialize(html, id)
+    @exec = Playwright.create playwright_cli_executable_path: "./node_modules/.bin/playwright"
+    @html = html
+    @id = id
+    @dir = DIR_PATH.join @id.to_s
+  end
+
+  def generate
+    cache = @dir.join(CACHE_PATH)
+    return if File.exist?(cache) && @html == File.read(cache, mode: "r:utf-8")
+
+    FileUtils.mkpath @dir
+    generate!
+    File.open(cache, "w:utf-8") { |f| f.write @html }
+  end
+
+  def generate!
+    @exec.playwright.chromium.launch do |browser|
+      page = browser.new_page viewport: { width: 1200, height: 630 }
+      page.goto URL.url("social_previews/article/#{@id}")
+      page.wait_for_function "() => document.fonts.check('28px IBM Plex Sans JP')" # check if a used webfont loaded
+      page.screenshot path: @dir.join(IMAGE_PATH)
+    end
+  ensure
+    @exec.stop
+  end
+
+  def url
+    generate
+    path = @dir.join(IMAGE_PATH)
+    File.exist?(path) ? URL.url("#{ROUTES.join '/'}/#{@id}/#{IMAGE_PATH}") : Settings::General.main_social_image.to_s
+  end
+end
