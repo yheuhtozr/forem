28f6e3dd6a184e2346835d01bc6b835babccf365
Yhëhtozr <conlang2012@outlook.com>
2022-05-15 15:24:44 +0900

    extend profile decorator to emit attribute key

--- app/decorators/profile_decorator.rb
+++ app/decorators/profile_decorator.rb
@@ -1,8 +1,9 @@
 class ProfileDecorator < ApplicationDecorator
   # Return a Hash of the profile fields that should be rendered for a given
   # display area, e.g. :left_sidebar
-  def ui_attributes_for(area:)
-    fields = ProfileField.public_send(area).pluck(:label, :attribute_name).to_h
+  def ui_attributes_for(area:, raw: false)
+    plucked = raw ? %i[attribute_name attribute_name] : %i[label attribute_name]
+    fields = ProfileField.public_send(area).pluck(*plucked).to_h
     fields.transform_values { |attribute_name| data[attribute_name] }.compact_blank
   end
 end
