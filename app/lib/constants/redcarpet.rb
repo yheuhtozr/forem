c76168e09eb5409846c2b7978c3c8e176a9bef4a
Yhëhtozr <conlang2012@outlook.com>
2022-12-02 20:28:29 +0900

    site function: better markdown options

--- app/lib/constants/redcarpet.rb
+++ app/lib/constants/redcarpet.rb
@@ -2,12 +2,12 @@ module Constants
   module Redcarpet
     CONFIG = {
       autolink: true,
-      no_intra_emphasis: true,
+      no_intra_emphasis: false, # disabled, not suitable for linguistic contents
       fenced_code_blocks: true,
       lax_html_blocks: true,
       lax_spacing: true,
       strikethrough: true,
-      superscript: false,
+      superscript: true, # enabled
       tables: true,
       footnotes: true
     }.freeze
