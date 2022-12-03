8c8535f88b0b8552e7340be3e7cb852c879b8d79
Yhëhtozr <conlang2012@outlook.com>
2022-12-03 19:22:02 +0900

    slug m17n nasty typo hotfix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -173,7 +173,7 @@ class CommentsController < ApplicationController
     redirect = @comment.commentable&.path || user_path(current_user)
     # NOTE: Brakeman doesn't like redirecting to a path, because of a "possible
     # unprotected redirect". Using URI.parse().path is the recommended workaround.
-    redirect_to Addressable::URI.encode(redirect, Addresable::URI).path,
+    redirect_to Addressable::URI.encode(redirect, Addressable::URI).path,
                 notice: I18n.t("comments_controller.delete.notice")
   end
 
